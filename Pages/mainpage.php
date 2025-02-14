51270b7a809a968da002e3b23f2487f3740c6860
iancallo1 <ian.galaroza.callo@gmail.com>
2025-02-15 03:17:26 +0800

    extra X button removed| added todo list

--- Pages/mainpage.php
+++ Pages/mainpage.php
@@ -97,7 +97,7 @@ include ('../dbcon.php');?>
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel" >Add Students</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+          
         </button>
       </div>
       <div class="modal-body">
